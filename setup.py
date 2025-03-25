6cfb9be7fe51a692f3bb6c6c8a9650738012bf70
HMUNACHI <ndubuakuhenry@gmail.com>
2025-03-25 11:12:42 +0000

    v0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cactus-lib",
-    version="0.1.3",
+    version="0.1.4",
     description="Framework for fine-tuning LLMs on the Cactus Compute platform",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
